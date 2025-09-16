bf6ae14959a186b805a7de7a204dab655f356fed
Sanou Mohamed Bachir <sanoumohamed97@gmail.com>
2025-09-16 09:24:43 +0800

    Initial commit

---
+++ src/main/java/dev/lordbach/movieAppApi/MovieAppApiApplication.java
@@ -0,0 +1,13 @@
+package dev.lordbach.movieAppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lordbach/movieAppApi/MovieAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lordbach.movieAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
